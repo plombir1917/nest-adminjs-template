86c666139c87bac6ab70d3abb16a8a293b69cf92
KopichZS <KopichZS@admlr.lipetsk.ru>
2025-08-22 16:52:56 +0300

    feat(main.ts): whitelist for validation pipe; public global dirname

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,21 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module.js';
 import { ValidationPipe } from '@nestjs/common';
+import * as express from 'express';
+import { join, dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  app.useGlobalPipes(new ValidationPipe());
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+    }),
+  );
+  app.use('/public', express.static(join(__dirname, '..', 'public')));
   await app.listen(3002);
 }
 bootstrap();
