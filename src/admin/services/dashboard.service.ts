85ed1eade30b0b02dc61df7971fea65a7c59a176
KopichZS <KopichZS@admlr.lipetsk.ru>
2025-08-28 15:53:19 +0300

    feat(dashboard.service): get dashboard method

--- src/admin/services/dashboard.service.ts
+++ src/admin/services/dashboard.service.ts
@@ -1,9 +1,17 @@
 import { Injectable } from '@nestjs/common';
-import { UserService } from 'src/api/user/user.service.js';
+import { UserService } from '../../api/user/user.service.js';
+import { Components } from '../components/components.config.js';
 
 @Injectable()
 export class DashboardService {
   constructor(private readonly userService: UserService) {}
+
+  getDashboard() {
+    return {
+      component: Components.Dashboard,
+      handler: async () => await this.getDashboardMetrics(),
+    };
+  }
 
   async getDashboardMetrics() {
     const usersCount = await this.userService.count();
