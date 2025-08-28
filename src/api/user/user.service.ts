15d8fd81df3781d99755e11689b289c9fda95bf1
KopichZS <KopichZS@admlr.lipetsk.ru>
2025-08-28 15:37:25 +0300

    feat(user.service): try/catch for every method

--- src/api/user/user.service.ts
+++ src/api/user/user.service.ts
@@ -2,27 +2,60 @@ import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto.js';
 import { UpdateUserDto } from './dto/update-user.dto.js';
 import { UserRepository } from './user.repository.js';
+import { CreateError } from '../errors/create.error.js';
+import { FindError } from '../errors/find.error.js';
+import { UpdateError } from '../errors/update.error.js';
+import { DeleteError } from '../errors/delete.error.js';
 
 @Injectable()
 export class UserService {
   constructor(private readonly userRepository: UserRepository) {}
+
   async create(createUserDto: CreateUserDto) {
-    return await this.userRepository.create(createUserDto);
+    try {
+      return await this.userRepository.create(createUserDto);
+    } catch (error) {
+      throw new CreateError('User');
+    }
   }
 
   async findAll() {
-    return await this.userRepository.findAll();
+    try {
+      return await this.userRepository.findAll();
+    } catch (error) {
+      throw new FindError('User');
+    }
   }
 
   async findOne(id: number) {
-    return await this.userRepository.findOne(id);
+    try {
+      return await this.userRepository.findOne(id);
+    } catch (error) {
+      throw new FindError('User');
+    }
   }
 
   async update(id: number, updateUserDto: UpdateUserDto) {
-    return await this.userRepository.update(id, updateUserDto);
+    try {
+      return await this.userRepository.update(id, updateUserDto);
+    } catch (error) {
+      throw new UpdateError('User');
+    }
   }
 
   async delete(id: number) {
-    return await this.userRepository.delete(id);
+    try {
+      return await this.userRepository.delete(id);
+    } catch (error) {
+      throw new DeleteError('User');
+    }
+  }
+
+  async count() {
+    try {
+      return await this.userRepository.count();
+    } catch (error) {
+      throw new Error(error.message);
+    }
   }
 }
