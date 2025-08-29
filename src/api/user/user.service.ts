01c4db7d8fd70b47c6b6ef70f514bcde9b98e119
KopichZS <KopichZS@admlr.lipetsk.ru>
2025-08-29 16:43:34 +0300

    feat(user): bcrypt integration

--- src/api/user/user.service.ts
+++ src/api/user/user.service.ts
@@ -6,6 +6,7 @@ import { CreateError } from '../errors/create.error.js';
 import { FindError } from '../errors/find.error.js';
 import { UpdateError } from '../errors/update.error.js';
 import { DeleteError } from '../errors/delete.error.js';
+import { encodePassword } from '../../utils/bcrypt.js';
 
 @Injectable()
 export class UserService {
@@ -13,6 +14,7 @@ export class UserService {
 
   async create(createUserDto: CreateUserDto) {
     try {
+      createUserDto.password = await encodePassword(createUserDto.password);
       return await this.userRepository.create(createUserDto);
     } catch (error) {
       throw new CreateError('User');
@@ -35,8 +37,19 @@ export class UserService {
     }
   }
 
+  async findOneByEmail(email: string) {
+    try {
+      return await this.userRepository.findOneBy({ email: email });
+    } catch (error) {
+      throw new FindError('User');
+    }
+  }
+
   async update(id: number, updateUserDto: UpdateUserDto) {
     try {
+      if (updateUserDto.password)
+        updateUserDto.password = await encodePassword(updateUserDto.password);
+
       return await this.userRepository.update(id, updateUserDto);
     } catch (error) {
       throw new UpdateError('User');
