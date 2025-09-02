d068c80c22b0441ea8c24f99d8f5132d2aea55d5
KopichZS <KopichZS@admlr.lipetsk.ru>
2025-09-02 14:20:50 +0300

    feat(user.module): `AuthService` as a provider

--- src/api/user/user.module.ts
+++ src/api/user/user.module.ts
@@ -3,11 +3,12 @@ import { UserService } from './user.service.js';
 import { UserController } from './user.controller.js';
 import { PrismaModule } from '../../prisma/prisma.module.js';
 import { UserRepository } from './user.repository.js';
+import { AuthService } from '../auth/auth.service.js';
 
 @Module({
   imports: [PrismaModule],
   controllers: [UserController],
-  providers: [UserService, UserRepository],
+  providers: [UserService, UserRepository, AuthService],
   exports: [UserService],
 })
 export class UserModule {}
