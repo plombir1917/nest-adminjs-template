2a0b12140e8d9d1d1f1e5562d6e36f46846f3b17
plombir1917 <dfhhggh@list.ru>
2025-09-04 17:27:26 +0300

    feat(app.module): serve static module

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,23 @@ import { Module } from '@nestjs/common';
 import { UserModule } from './api/user/user.module.js';
 import { PrismaModule } from './prisma/prisma.module.js';
 import { AdminJSModule } from './admin/admin.module.js';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join, dirname } from 'path';
+import { fileURLToPath } from 'url';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 @Module({
-  imports: [AdminJSModule, PrismaModule, UserModule],
+  imports: [
+    AdminJSModule,
+    PrismaModule,
+    UserModule,
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'uploads'),
+      serveRoot: '/uploads',
+    }),
+  ],
   controllers: [],
   providers: [],
 })
