d76bf9f526059e6d463d9430b415746bdc08aa7c
plombir1917 <dfhhggh@list.ru>
2025-09-05 17:35:40 +0300

    feat: photo component

--- .adminjs/entry.js
+++ .adminjs/entry.js
@@ -1,6 +1,8 @@
 AdminJS.UserComponents = {}
 import Dashboard from '../src/admin/components/dashboard'
 AdminJS.UserComponents.Dashboard = Dashboard
+import Photo from '../src/admin/components/photo'
+AdminJS.UserComponents.Photo = Photo
 import UploadEditComponent from '../node_modules/@adminjs/upload/build/features/upload-file/components/UploadEditComponent'
 AdminJS.UserComponents.UploadEditComponent = UploadEditComponent
 import UploadListComponent from '../node_modules/@adminjs/upload/build/features/upload-file/components/UploadListComponent'
