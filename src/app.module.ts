5c13a3bc9502324d9c6743b3c32d7ffa5f9d5b96
plombir1917 <dfhhggh@list.ru>
2025-09-05 16:45:37 +0300

    delete(package): @nestjs/serve-static

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,23 +2,9 @@ import { Module } from '@nestjs/common';
 import { UserModule } from './api/user/user.module.js';
 import { PrismaModule } from './prisma/prisma.module.js';
 import { AdminJSModule } from './admin/admin.module.js';
-import { ServeStaticModule } from '@nestjs/serve-static';
-import { join, dirname } from 'path';
-import { fileURLToPath } from 'url';
-
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
 
 @Module({
-  imports: [
-    AdminJSModule,
-    PrismaModule,
-    UserModule,
-    ServeStaticModule.forRoot({
-      rootPath: join(__dirname, '..', 'uploads'),
-      serveRoot: '/uploads',
-    }),
-  ],
+  imports: [AdminJSModule, PrismaModule, UserModule],
   controllers: [],
   providers: [],
 })
