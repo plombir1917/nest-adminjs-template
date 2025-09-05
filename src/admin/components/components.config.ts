ff3ca20661af3c4c333f100f81be5a71d9a6bcf4
plombir1917 <dfhhggh@list.ru>
2025-09-05 17:35:10 +0300

    feat: photo component

--- src/admin/components/components.config.ts
+++ src/admin/components/components.config.ts
@@ -4,4 +4,5 @@ export const componentLoader = new ComponentLoader();
 
 export const Components = {
   Dashboard: componentLoader.add('Dashboard', './dashboard'),
+  Photo: componentLoader.add('Photo', './photo'),
 };
