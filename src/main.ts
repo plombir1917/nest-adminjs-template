d796cf2cda374127ecb75a16f413de75be9a84c1
plombir1917 <dfhhggh@list.ru>
2025-09-12 15:30:17 +0300

    feat(main.ts): swagger module init

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,7 @@ import { AppModule } from './app.module.js';
 import { ValidationPipe } from '@nestjs/common';
 import * as express from 'express';
 import { join } from 'path';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
@@ -11,6 +12,17 @@ async function bootstrap() {
       whitelist: true,
     }),
   );
+
+  const config = new DocumentBuilder()
+    .setTitle('Область будущего')
+    .setDescription('Документация для системы администрирования')
+    .setVersion('1.0')
+    .build();
+  const documentFactory = () => SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('docs', app, documentFactory, {
+    swaggerOptions: { defaultModelsExpandDepth: -1 },
+  });
+
   app.use('/public', express.static(join(process.cwd(), 'public')));
   await app.listen(3002);
 }
